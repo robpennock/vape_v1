8c3a558228754dadf5281f6b743d3961639d87c0
rob pennock <robpennock@gmail.com>
2015-05-08 14:19:12 -0700

    added final pics and text updates for release

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,64 +10,66 @@ class WelcomeController < ApplicationController
 		#strings of tag text for carousel captions
 		@carTagText = ["<h1>Welcome to elevape</h1>
 					</br>
-					<h3>Home of Vallejo's Finest Vapor Products</h3>
+					<h2>Home of Vallejo's Finest Vapor Products</h2>
 					</br><h2>Opening Soon!</h2>", 
-					"<h1>Organic and Locally Made</h1>
-		  			<h3>For the cleanest experience</h3>",
+					"<h1>High Quality and Locally Made Products</h1>
+		  			<h2>For the cleanest experience</h2>",
           			"<h1>Visit Us At</h1>
-		  			<h3><b>419</b> Georgia st Suite <b>9</b></h3>
-		  			<h3>Vallejo, CA</h3>
-		  			<h3>PH: 805-354-9692</h3>
-		  			<h3>Hours 11am - 730pm Daily</h3>" ]
+		  			<h2>419 Georgia st Suite 9</h2>
+		  			<h2>Vallejo, CA</h2>
+		  			<h2>PH: 805-354-9692</h2>
+		  			<h2>Hours 11am - 730pm Daily</h2>" ]
 	end
 
 	def fluids
-		@vImages = [ "high_voltage_family.jpg", "high_voltage_family2.jpg", "fluid3.jpg" ]
+		@vImages = [ "high_voltage_family3.jpg", "high_voltage_family2.jpg", "juicyohms3.jpg" ]
 		@imgSize = "800x600"
 		@carTagText = ["<h1>At elevape</h1>
-					</br>
-					<h3>We <i>Only</i> Carry Finest Vapor Products</h3>
+					<h2>We <i>Only</i> Carry Finest Vapor Products</h2>
 					</br>", 
 					"<h1>Now Carrying</h1>
-		  			<h3>High Voltage</h3>
-		  			<h3>Royal</h3>
-		  			<h3>Cuttwood</h3>
-		  			<h3>Juicy Ohms</h3>
-		  			<h3>Nicobine</h3>",
+		  			<h2>High Voltage</h2>
+		  			<h2>Royal</h2>
+		  			<h2>Cuttwood</h2>
+		  			<h2>Juicy Ohms</h2>
+		  			<h2>Nicobine</h2>",
           			"<h1>Visit Us At</h1>
-		  			<h3>419 Georgia st Suite 9</h3>
-		  			<h3>Vallejo, CA</h3>
-		  			<h3>PH: 805-354-9692</h3>
-		  			<h3>Hours 11am - 730pm Daily</h3>" ]
+		  			<h2>419 Georgia st Suite 9</h2>
+		  			<h2>Vallejo, CA</h2>
+		  			<h2>PH: 805-354-9692</h2>
+		  			<h2>Hours 11am - 730pm Daily</h2>" ]
 	end
 
 	def kits
-		@vImages = [ "vaporrev1.jpg", "vaporrev2.jpg", "vaporrev3.jpg" ]
+		@vImages = [ "Aspire-Atlantis-disassembled.jpg", "lotus-le80-2.jpg", "efest.jpg" ]
 		@imgSize = "960x640"
 		@carTagText = ["<h1>At elevape</h1>
 					</br>
-					<h3>We <i>Only</i> Carry Finest Vapor Products</h3>
+					<h2>We <i>Only</i> Carry Finest Vapor Kits, Mods, and Batteries</h2>
 					</br>", 
-					"<h1>We Carry Only the Highest Quality</h1>
-          			<h3>Vapor Kits and Mods</h3>",
+					"<h1>We Proudly Carry</h1>
+					<h2>Aspire</h2>
+          			<h2>Lotus</h2>
+          			<h2>MXJO</h2>
+          			<h2>Efest</h2>",
           			"<h1>Visit Us At</h1>
-		  			<h3>419 Georgia st Suite 9</h3>
-		  			<h3>Vallejo, CA</h3>
-		  			<h3>PH: 805-354-9692</h3>
-		  			<h3>Hours 11am - 730pm Daily</h3>" ]
+		  			<h2>419 Georgia st Suite 9</h2>
+		  			<h2>Vallejo, CA</h2>
+		  			<h2>PH: 805-354-9692</h2>
+		  			<h2>Hours 11am - 730pm Daily</h2>" ]
 	end
 
 	def about
-		@vImages = [ "big-family1.jpg", "big-family2.jpg", "big-family2.jpg" ]
-		@imgSize = "1300x866"
+		@vImages = [ "spence_climb.jpg", "nate_spence.jpg", "nate.jpg" ]
+		@imgSize = "640x640"
 		@carTagText = ["<h1><b>elevape</b> is Family Owned and Operated</h1>
 					</br>", 
 					"<h1>Come by and visit us anytime!</h1>
           			</br>",
           			"<h1>Visit Us At</h1>
-		  			<h3>419 Georgia st Suite 9</h3>
-		  			<h3>Vallejo, CA</h3>
-		  			<h3>PH: 805-354-9692</h3>
-		  			<h3>Hours 11am - 730pm Daily</h3>" ]
+		  			<h2>419 Georgia st Suite 9</h2>
+		  			<h2>Vallejo, CA</h2>
+		  			<h2>PH: 805-354-9692</h2>
+		  			<h2>Hours 11am - 730pm Daily</h2>" ]
 	end
 end
